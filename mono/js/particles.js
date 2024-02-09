d61b7860def50e9b6a977ada1822bc5ce4ad8f3d
LuisNego <luishenriquefshenri@gmail.com>
2024-02-09 06:54:28 -0400

    Quase todas as mudanças estão salvas
    Mudanças que ocorreram durante os períodos de aula

--- mono/js/particles.js
+++ mono/js/particles.js
@@ -134,7 +134,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#b61924",
+      "background_color": "#00000",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
